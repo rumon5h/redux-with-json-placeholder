5124a1216ba192d118958caa79f9f63173bf4ed9
rumon5 <mdrumonkhan48519@gmail.com>
2022-07-19 22:11:54 +0600

    State implementation done

--- index.js
+++ index.js
@@ -6,3 +6,10 @@
 const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST';
 const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS';
 const GET_TODOS_FAILED = 'GET_TODOS_FAILED';
+
+// Create state 
+const initialTodosState = {
+    todos: [],
+    isLoading: false,
+    error: null
+}
