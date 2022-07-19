44376d49e050b5be62606262e9b637ec3f2029a9
rumon5 <mdrumonkhan48519@gmail.com>
2022-07-19 22:10:43 +0600

    Added constant

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // https://jsonplaceholder.typicode.com/todos
+
+
+// Constant 
+
+const GET_TODOS_REQUEST = 'GET_TODOS_REQUEST';
+const GET_TODOS_SUCCESS = 'GET_TODOS_SUCCESS';
+const GET_TODOS_FAILED = 'GET_TODOS_FAILED';
